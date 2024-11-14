2280189bfcb01c5274471e912b35dab35ded31b4
Jakub Pajak <jakubpajak02@outlook.com>
2024-11-14 13:49:51 +0100

    Ros2 package added

---
+++ build/object_coords/CMakeFiles/object_coords.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/object_coords.dir/src/object_coords_publisher.cpp.o"
+  "CMakeFiles/object_coords.dir/src/object_coords_publisher.cpp.o.d"
+  "object_coords"
+  "object_coords.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/object_coords.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/object_coords/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosdev/ros2_ws/object_coords
+# Build directory: /home/rosdev/ros2_ws/build/object_coords
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
